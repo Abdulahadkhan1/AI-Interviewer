cb2552ca0ee14be1b35ef92cdf2a3be0d358e3d1
Abdulahadkhan1 <abdulahadhan168@gmail.com>
2025-11-01 15:24:23 +0500

    Home page build

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,45 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      {/* Add your content here */}
-      <h1>Home Page</h1>
-    </div>
+    <>  
+      <section className="card-cta">
+      <div className="flex flex-col gap-6 max-w-lg">
+        <h2>Get Interview Ready with AI-Powered Practice & Feedback</h2>
+        <p className="text-muted-foreground">
+          Practice on Real Interview questions, Get Instant AI Feedback, and
+          Improve Your Skills.
+        </p>
+        <Button asChild className="btn-primary max-small:w-full">
+          <Link href="/interview" >Start an Interview</Link>
+        </Button>
+      </div>
+
+      <Image 
+      src="/robot.png" 
+      alt="robo-dude" 
+      width={400} 
+      height={400} 
+      className="max-sm:hidden" />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2 className="">Your Interview</h2>
+
+        <div className="interviews-section">
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2 className="">Take an Interview</h2>
+
+        <div className="interviews-section">
+          <p>There are no interviews available</p>
+        </div>
+      </section>
+    </>
   );
 }
